a2ea1edd96cc52c3193428982d0741c34e3d3226
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-09-18 00:39:54 +0500

    Add files via upload

---
+++ Class_9 array methods/main.js
@@ -0,0 +1,26 @@
+"use strict";
+let beautyproducts = ["primer", "foundation", "face powder", "eyeshadow", "blush", "lipstick", "mascara", "liner"];
+// console.log(beautyproducts.length);
+// console.log(beautyproducts);
+// console.log(beautyproducts.push("concealer"));
+// console.log(beautyproducts);
+// console.log(beautyproducts.pop());
+// console.log(beautyproducts.unshift("nailpolish"));
+// console.log(beautyproducts);
+// console.log(beautyproducts.shift());
+// console.log(beautyproducts.slice(2,2));
+// console.log(beautyproducts);
+// beautyproducts.slice(3,6)
+// let sliceArray=beautyproducts.slice(3,6);
+// console.log(sliceArray);
+// beautyproducts.slice(0,1);
+// let sliceArray1=beautyproducts.slice(0,1);
+// console.log(sliceArray1);
+// beautyproducts.pop();
+// let popArray=beautyproducts.pop()
+// console.log(popArray);
+beautyproducts.push("fixer");
+console.log(beautyproducts);
+beautyproducts.shift();
+let sliceArray = beautyproducts.shift();
+console.log(sliceArray);
---
+++ Class_9 array methods/main.ts
@@ -0,0 +1,49 @@
+let beautyproducts : string[] = ["primer","foundation","face powder","eyeshadow","blush","lipstick","mascara","liner"];
+console.log(beautyproducts.length);
+
+console.log(beautyproducts);
+
+console.log(beautyproducts.push("concealer"));
+
+console.log(beautyproducts);
+
+console.log(beautyproducts.pop());
+
+
+console.log(beautyproducts.unshift("nailpolish"));
+console.log(beautyproducts);
+console.log(beautyproducts.shift());
+console.log(beautyproducts.slice(2,2));
+console.log(beautyproducts);
+
+beautyproducts.slice(3,6)
+let sliceArray0=beautyproducts.slice(3,6);
+console.log(sliceArray0);
+
+beautyproducts.slice(0,1);
+let sliceArray1=beautyproducts.slice(0,1);
+console.log(sliceArray1);
+
+beautyproducts.pop();
+let popArray=beautyproducts.pop()
+console.log(popArray);
+
+
+beautyproducts.push("fixer");
+
+console.log(beautyproducts);
+
+beautyproducts.shift();
+let sliceArray=beautyproducts.shift()
+console.log(sliceArray);
+
+
+
+
+
+
+
+
+
+
+
